f348227efc0306567c3011fbf3422e15f8bde915
Jim Haff <Jimmyhaff3@gmail.com>
2018-09-21 17:57:54 +0100

    applyRandomColor function working

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -13,12 +13,37 @@ $('button').on('click', () => {
 
 const createSquares = (numberOfSquares) => {
 
-  for (let i = 0; i <= numberOfSquares; i++){
+  for (let i = 0; i < numberOfSquares; i++){
     // creating squares
     const $div = $('<div/>');
+    // this is where the color is applied
+    applyRandomColor($div);
     // attach to the square class
     $('.squares').append($div);
 
   }
+};
+
+//Now we have a bunch of blue squares,
+// but we want our squares to have a random,
+// red, blue, or green color
+// try to write a function to make that happen.
+
+const applyRandomColor = (square) => {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  // im assuming square is a jquery object
+
+  if(randNum === 1){
+    square.css('background-color', 'red');
+  } else if(randNum === 2){
+    square.css('background-color', 'blue');
+  } else {
+    square.css('background-color', 'green');
+  }
+
 
 }
+
+
+
