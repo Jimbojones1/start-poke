7d7b873ddcc40cb76cb986338617f92797aa9b3d
Jim Haff <Jimmyhaff3@gmail.com>
2018-09-21 17:31:51 +0100

    createSquare function is working

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,24 @@
 
+$('button').on('click', () => {
+  console.log('click works');
+  // When the user clicks the button, populate the squares
+  createSquares(30);
+
+});
+
+
+// Create a function named createSquares, that takes
+// a parameter of an arbitary number and creates
+// divs according to the number in the square class
+
+const createSquares = (numberOfSquares) => {
+
+  for (let i = 0; i <= numberOfSquares; i++){
+    // creating squares
+    const $div = $('<div/>');
+    // attach to the square class
+    $('.squares').append($div);
+
+  }
+
+}
